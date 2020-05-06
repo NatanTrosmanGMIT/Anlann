ae469c5152ee2eb2a9881ea9090e2edf34df6806
NatanTrosmanGMIT <61838651+NatanTrosmanGMIT@users.noreply.github.com>
2020-05-06 15:47:06 +0100

    Add files via upload

---
+++ AnlannMaster/login.js
@@ -0,0 +1,35 @@
+	// Loop through Array of Objects
+var objPeople = [
+	{ // Object @ 0 index
+		username: "John",
+		password: "Doe"
+	},
+	{ // Object @ 1 index
+		username: "123",
+		password: "123"
+	},
+	{ // Object @ 2 index
+		username: "Michael",
+		password: "Duignan"
+	}
+
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			document.getElementById("login").innerHTML = "My Account";
+			alert(username + " is logged in")
+			console.log(username + " is logged in")
+			//window.location.href = "homePage.html";
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	alert("incorrect username or password")
+	console.log("incorrect username or password")
+}
